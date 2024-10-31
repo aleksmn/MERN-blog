66b66017449cbd68af8b51779d8b7594965d1ff9
aleksmn <al-mn@yandex.ru>
2024-10-31 11:43:17 +0300

    add navbar styles

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <h3>MERN Blog App</h3>
+            <div>
+                <a href="" className='link'>Home</a>
+                <a href="" className='link'>Create</a>
+                <a href="" className='link'>Contact</a>
+            </div>
+            <div>Register/Login</div>
+        </nav>
+    )
+}
+
+
+export default Navbar
