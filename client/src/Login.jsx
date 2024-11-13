83de52ed64b91fa5802b06b4d2b7952807926451
aleksmn <al-mn@yandex.ru>
2024-11-13 18:45:40 +0300

    fix import problems

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 function Login() {
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
-    const navigate = useNavigate()
 
     axios.defaults.withCredentials = true;
     const handleSubmit = (e) => {
