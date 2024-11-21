e72ba716730532eaadc49fe89f8036b4efba5a6a
aleksmn <al-mn@yandex.ru>
2024-11-21 11:47:30 +0300

    add create page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import Navbar from "./Navbar"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import axios from "axios"
+import { createContext, useEffect, useState } from "react"
+import Navbar from "./Navbar"
 import Register from "./Register"
 import Login from "./Login"
 import Home from "./Home"
-import axios from "axios"
-import { createContext, useEffect, useState } from "react"
+import CreatePost from "./CreatePost"
+
 
 export const userContext = createContext()
 
@@ -31,6 +33,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/create" element={<CreatePost />}></Route>
       </Routes>
     </BrowserRouter>
     </userContext.Provider>
